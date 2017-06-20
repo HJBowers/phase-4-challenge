4f3cbc3b2a4646880025f9c1b5d7a78539ede39e
HJBowers <HJBowers@gmail.com>
2017-06-19 18:01:45 -0700

    updated seed data and added user table and reviews table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,24 @@
 CREATE TABLE albums (
   id SERIAL,
   title VARCHAR(255) NOT NULL,
-  artist VARCHAR(255) NOT NULL
+  artist VARCHAR(255) NOT NULL,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE users (
+  id SERIAL,
+  name VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  join_date date NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE reviews (
+  review_id int NOT NULL PRIMARY KEY,
+  album_id int NOT NULL,
+  user_id int NOT NULL,
+  review text NOT NULL,
+  FOREIGN KEY (album_id) REFERENCES albums (id),
+  FOREIGN KEY (user_id) REFERENCES users (id)
 );
